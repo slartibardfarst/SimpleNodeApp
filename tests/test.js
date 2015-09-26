950d1d9923fbcffc8e09321cef2518b9d278daf4
Watkins, Andrew <Andrew.Watkins@move.com>
2015-09-26 10:03:41 -0700

    renamed test

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ describe('Array', function() {
   });
 });
 
-describe('Array', function() {
+describe('AnotherArray', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
