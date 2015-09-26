8bdca180018abd1bd7a840f0d6fc1f7072106cc8
Watkins, Andrew <Andrew.Watkins@move.com>
2015-09-26 09:58:49 -0700

    added another test

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,5 @@
 var assert = require("assert");
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,4 +7,13 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+
 });
